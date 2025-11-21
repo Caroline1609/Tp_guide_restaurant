77722b3a68bff8cc419324aeabc680a8974387a6
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-11-21 09:46:41 +0100

    upload

--- index.php
+++ index.php
@@ -1,25 +1,46 @@
+<?php
+require "./dao/DbConnection.php";
+// 1. Connexion à la base
+$dbConnexion = DbConnection::getInstance();
+$pdo = $dbConnexion->getConnection();
+
+$reponse = $pdo->query("SELECT * FROM restaurant");
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>restaurant</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-<?php
-require "./dao/DbConnection.php";
-
-$dbConnexion = DbConnection::getInstance();
-
-var_dump($dbConnexion);
-
-?>   
-
-
-
-
-
-
+<h1>Liste des Restaurants</h1>
+<main>
+    <table>
+    <tr>
+        <th>ID</th>
+        <th>Nom</th>
+        <th>Adresse</th>
+        <th>Prix</th>
+        <th>Commentaire</th>
+        <th>Note</th>
+        <th>Visite</th>
+    </tr>
+    <?php while ($donnees = $reponse->fetch()): ?>
+        <tr>
+            <td><?= $donnees['id'] ?></td>
+            <td><?= $donnees['nom'] ?></td>
+            <td><?= $donnees['adresse'] ?></td>
+            <td><?= $donnees['prix'] ?> €</td>
+            <td><?= $donnees['commentaire'] ?></td>
+            <td><?= $donnees['note'] ?>/5</td>
+            <td><?= $donnees['visite'] ?></td>
+        </tr>
+    <?php endwhile; ?>
+</table>
+</main>
 
 
 </body>

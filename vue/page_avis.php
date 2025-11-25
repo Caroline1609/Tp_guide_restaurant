194843e48483326b50368005b98b46ee5774a30a
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-11-25 14:53:42 +0100

    upload

--- vue/page_avis.php
+++ vue/page_avis.php
@@ -1,5 +1,5 @@
 <?php
-    $title = "Ajouter un restaurant";
+    $title = "Tous les avis des restaurants";
     require __DIR__ . '/../header.php';
     require_once __DIR__ . '/../src/dao/RestaurantRepository.php';
     $repo = new RestoRepository(); //Créer une instance du repository
@@ -9,6 +9,18 @@
 
 <main>
     <h1>Liste des avis</h1>
+
+    <?php if (isset($_GET['success'])): ?> // Affichage du message de succès ?>
+        <div class="success"> 
+            ✅ <?= htmlspecialchars($_GET['success']) ?>
+        </div>
+    <?php endif; ?>
+    
+    <?php if (isset($_GET['error'])): ?> // Affichage du message d'erreur ?>
+        <div class="error">
+            ❌ <?= htmlspecialchars($_GET['error']) ?> 
+        </div>
+    <?php endif; ?>
 
     <?php
     // Génère et affiche le tableau
